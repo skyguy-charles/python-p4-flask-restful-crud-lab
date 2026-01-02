f791e7ab19bf5ce9f293532eb212b723bc1900ee
charles <cyphersmae@gmail.com>
2026-01-02 22:56:03 +0300

    new changes

--- server/app.py
+++ server/app.py
@@ -1,8 +1,6 @@
-#!/usr/bin/env python3
-
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, request, make_response, jsonify
+from flask_cors import CORS
 from flask_migrate import Migrate
-from flask_restful import Api, Resource
 
 from models import db, Plant
 
@@ -11,45 +9,165 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
+CORS(app)
 migrate = Migrate(app, db)
+
 db.init_app(app)
 
-api = Api(app)
+@app.route('/')
+def index():
+    return '<h1>Plant Store API</h1>'
 
+# GET all plants
+@app.route('/plants', methods=['GET'])
+def get_plants():
+    plants = Plant.query.all()
+    plants_list = [plant.to_dict() for plant in plants]
+    return jsonify(plants_list), 200
 
-class Plants(Resource):
+# GET one plant by ID
+@app.route('/plants/<int:id>', methods=['GET'])
+def get_plant(id):
+    plant = Plant.query.filter_by(id=id).first()
+    if plant:
+        return jsonify(plant.to_dict()), 200
+    return jsonify({"error": "Plant not found"}), 404
 
-    def get(self):
-        plants = [plant.to_dict() for plant in Plant.query.all()]
-        return make_response(jsonify(plants), 200)
+# POST create a new plant
+@app.route('/plants', methods=['POST'])
+def create_plant():
+    data = request.get_json()
+    new_plant = Plant(
+        name=data.get('name'),
+        image=data.get('image'),
+        price=data.get('price'),
+        is_in_stock=data.get('is_in_stock', True)
+    )
+    db.session.add(new_plant)
+    db.session.commit()
+    return jsonify(new_plant.to_dict()), 201
 
-    def post(self):
-        data = request.get_json()
+# PATCH update a plant
+@app.route('/plants/<int:id>', methods=['PATCH'])
+def update_plant(id):
+    plant = Plant.query.filter_by(id=id).first()
+    
+    if not plant:
+        return jsonify({"error": "Plant not found"}), 404
+    
+    data = request.get_json()
+    
+    # Update only the fields that are provided
+    for key in data:
+        setattr(plant, key, data[key])
+    
+    db.session.commit()
+    
+    return jsonify(plant.to_dict()), 200
 
-        new_plant = Plant(
-            name=data['name'],
-            image=data['image'],
-            price=data['price'],
-        )
-
-        db.session.add(new_plant)
-        db.session.commit()
-
-        return make_response(new_plant.to_dict(), 201)
-
-
-api.add_resource(Plants, '/plants')
-
-
-class PlantByID(Resource):
-
-    def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
-
-
-api.add_resource(PlantByID, '/plants/<int:id>')
+# DELETE a plant
+@app.route('/plants/<int:id>', methods=['DELETE'])
+def delete_plant(id):
+    plant = Plant.query.filter_by(id=id).first()
+    
+    if not plant:
+        return jsonify({"error": "Plant not found"}), 404
+    
+    db.session.delete(plant)
+    db.session.commit()
+    
+    # Return empty response with 204 status code
+    return '', 204
 
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- server/models.py
+++ server/models.py
@@ -10,7 +10,35 @@ class Plant(db.Model, SerializerMixin):
     name = db.Column(db.String)
     image = db.Column(db.String)
     price = db.Column(db.Float)
-    is_in_stock = db.Column(db.Boolean)
+    is_in_stock = db.Column(db.Boolean, default=True)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'image': self.image,
+            'price': self.price,
+            'is_in_stock': self.is_in_stock
+        }
 
     def __repr__(self):
-        return f'<Plant {self.name} | In Stock: {self.is_in_stock}>'
+        return f'<Plant {self.name}>'
+ 
+
+ 
+ 
+ 
+ 
+ 
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+ 
+ 
